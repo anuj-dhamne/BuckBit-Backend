844488a4adde0dae59304499e1097b1e87cfd5bc
anuj-dhamne <anujdhamne10@gmail.com>
2025-07-05 16:07:16 +0530

    commit for deployment

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import './utils/reminderCron.js'
 const app= express();
 
 app.use(cors({
-    // origin:process.env.CORS_ORIGIN,
-    origin:"http://localhost:5173" ||process.env.CORS_ORIGIN,
+    
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
 app.use(express.json())
